ea015e7cdcc740edadbca03af13c8b7840bd207e
mat <mat.gautron@gmail.com>
2022-08-12 07:08:52 +0100

    Fix hash function error handling

--- util/hash.go
+++ util/hash.go
@@ -3,11 +3,17 @@ package util
 import (
 	"crypto/sha1"
 	"encoding/hex"
+
+	"github.com/matg94/go-url-shortener/errorhandling"
 )
 
 func HashString(original string, hashLength int) string {
 	hasher := sha1.New()
-	hasher.Write([]byte(original))
+	_, err := hasher.Write([]byte(original))
+	if err != nil {
+		errorhandling.HandleError(err, "Hashing", original)
+		return ""
+	}
 	sha := hex.EncodeToString(hasher.Sum(nil)[:hashLength])
 	return sha
 }
